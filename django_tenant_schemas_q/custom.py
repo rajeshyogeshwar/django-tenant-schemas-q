52ef28748a812bccc9285494c3df0c8520251ae9
Rajesh Yogeshwar <rajesh_y@rollingarrays.com>
2020-03-23 19:36:24 +0530

    Added support for chaining and creating multiple async tasks in one go

---
+++ django_tenant_schemas_q/custom.py
@@ -0,0 +1,270 @@
+# local
+from django_tenant_schemas_q.utils import QUtilities
+
+from django_q.conf import Conf
+from django_q.brokers import get_broker
+
+
+class Iter(object):
+    """
+    An async task with iterable arguments customised for django_tenant_schemas_q
+    """
+
+    def __init__(
+        self,
+        func=None,
+        args=None,
+        kwargs=None,
+        cached=Conf.CACHED,
+        sync=Conf.SYNC,
+        broker=None,
+    ):
+        self.func = func
+        self.args = args or []
+        self.kwargs = kwargs or {}
+        self.id = ""
+        self.broker = broker or get_broker()
+        self.cached = cached
+        self.sync = sync
+        self.started = False
+
+    def append(self, *args):
+        """
+        add arguments to the set
+        """
+        self.args.append(args)
+        if self.started:
+            self.started = False
+        return self.length()
+
+    def run(self):
+        """
+        Start queueing the tasks to the worker cluster
+        :return: the task id
+        """
+        self.kwargs["cached"] = self.cached
+        self.kwargs["sync"] = self.sync
+        self.kwargs["broker"] = self.broker
+        self.id = QUtilities.add_async_tasks_from_iter(self.func, self.args, **self.kwargs)
+        self.started = True
+        return self.id
+
+    def result(self, wait=0):
+        """
+        return the full list of results.
+        :param int wait: how many milliseconds to wait for a result
+        :return: an unsorted list of results
+        """
+        if self.started:
+            return QUtilities.get_result(self.id, wait=wait, cached=self.cached)
+
+    def fetch(self, wait=0):
+        """
+        get the task result objects.
+        :param int wait: how many milliseconds to wait for a result
+        :return: an unsorted list of task objects
+        """
+        if self.started:
+            return QUtilities.fetch_task(self.id, wait=wait, cached=self.cached)
+
+    def length(self):
+        """
+        get the length of the arguments list
+        :return int: length of the argument list
+        """
+        return len(self.args)
+
+
+class Chain(object):
+    """
+    A sequential chain of tasks
+    """
+
+    def __init__(self, chain=None, group=None, cached=Conf.CACHED, sync=Conf.SYNC):
+        self.chain = chain or []
+        self.group = group or ""
+        self.broker = get_broker()
+        self.cached = cached
+        self.sync = sync
+        self.started = False
+
+    def append(self, func, *args, **kwargs):
+        """
+        add a task to the chain
+        takes the same parameters as async_task()
+        """
+        self.chain.append((func, args, kwargs))
+        # remove existing results
+        if self.started:
+            QUtilities.delete_task_group(self.group)
+            self.started = False
+        return self.length()
+
+    def run(self):
+        """
+        Start queueing the chain to the worker cluster
+        :return: the chain's group id
+        """
+        self.group = QUtilities.create_async_tasks_chain(
+            chain=self.chain[:],
+            group=self.group,
+            cached=self.cached,
+            sync=self.sync,
+            broker=self.broker,
+        )
+        self.started = True
+        return self.group
+
+    def result(self, wait=0):
+        """
+        return the full list of results from the chain when it finishes. blocks until timeout.
+        :param int wait: how many milliseconds to wait for a result
+        :return: an unsorted list of results
+        """
+        if self.started:
+            return QUtilities.get_result_group(
+                self.group, wait=wait, count=self.length(), cached=self.cached
+            )
+
+    def fetch(self, failures=True, wait=0):
+        """
+        get the task result objects from the chain when it finishes. blocks until timeout.
+        :param failures: include failed tasks
+        :param int wait: how many milliseconds to wait for a result
+        :return: an unsorted list of task objects
+        """
+        if self.started:
+            return QUtilities.fetch_task_group(
+                self.group,
+                failures=failures,
+                wait=wait,
+                count=self.length(),
+                cached=self.cached,
+            )
+
+    def current(self):
+        """
+        get the index of the currently executing chain element
+        :return int: current chain index
+        """
+        if not self.started:
+            return None
+        return QUtilities.get_group_count(self.group, cached=self.cached)
+
+    def length(self):
+        """
+        get the length of the chain
+        :return int: length of the chain
+        """
+        return len(self.chain)
+
+
+class AsyncTask(object):
+    """
+    an async task
+    """
+
+    def __init__(self, func, *args, **kwargs):
+        self.id = ""
+        self.started = False
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    @property
+    def broker(self):
+        return self._get_option("broker", None)
+
+    @broker.setter
+    def broker(self, value):
+        self._set_option("broker", value)
+
+    @property
+    def sync(self):
+        return self._get_option("sync", None)
+
+    @sync.setter
+    def sync(self, value):
+        self._set_option("sync", value)
+
+    @property
+    def save(self):
+        return self._get_option("save", None)
+
+    @save.setter
+    def save(self, value):
+        self._set_option("save", value)
+
+    @property
+    def hook(self):
+        return self._get_option("hook", None)
+
+    @hook.setter
+    def hook(self, value):
+        self._set_option("hook", value)
+
+    @property
+    def group(self):
+        return self._get_option("group", None)
+
+    @group.setter
+    def group(self, value):
+        self._set_option("group", value)
+
+    @property
+    def cached(self):
+        return self._get_option("cached", Conf.CACHED)
+
+    @cached.setter
+    def cached(self, value):
+        self._set_option("cached", value)
+
+    def _set_option(self, key, value):
+        if "q_options" in self.kwargs:
+            self.kwargs["q_options"][key] = value
+        else:
+            self.kwargs[key] = value
+        self.started = False
+
+    def _get_option(self, key, default=None):
+        if "q_options" in self.kwargs:
+            return self.kwargs["q_options"].get(key, default)
+        else:
+            return self.kwargs.get(key, default)
+
+    def run(self):
+        self.id = async_task(self.func, *self.args, **self.kwargs)
+        self.started = True
+        return self.id
+
+    def result(self, wait=0):
+
+        if self.started:
+            return QUtilities.get_result(self.id, wait=wait, cached=self.cached)
+
+    def fetch(self, wait=0):
+
+        if self.started:
+            return QUtilities.fetch_task(self.id, wait=wait, cached=self.cached)
+
+    def result_group(self, failures=False, wait=0, count=None):
+
+        if self.started and self.group:
+            return QUtilities.get_result_group(
+                self.group,
+                failures=failures,
+                wait=wait,
+                count=count,
+                cached=self.cached,
+            )
+
+    def fetch_group(self, failures=True, wait=0, count=None):
+
+        if self.started and self.group:
+            return QUtilities.fetch_task_group(
+                self.group,
+                failures=failures,
+                wait=wait,
+                count=count,
+                cached=self.cached,
+            )
--- django_tenant_schemas_q/utils.py
+++ django_tenant_schemas_q/utils.py
@@ -1,24 +1,102 @@
+# standard
+import uuid
+from multiprocessing import Value
+
 # django
 from django.db import connection
+from django.utils import timezone
 
 # local
 from django_q.conf import Conf, logger
+from django_q.brokers import get_broker
+from django_q.signing import SignedPackage
+from django_q.queues import Queue
+from django_q.signals import pre_enqueue
 from tenant_schemas.utils import schema_context
-from django_q.tasks import schedule, async_task, result, \
-    result_group, fetch, fetch_group, count_group, delete_group, \
-    delete_cached, queue_size
+from django_q.tasks import (schedule,
+                            async_task,
+                            result,
+                            result_group,
+                            fetch,
+                            fetch_group,
+                            count_group,
+                            delete_group,
+                            delete_cached,
+                            queue_size)
 
 
 class QUtilities(object):
 
     @staticmethod
+    def prepare_task(func, *args, **kwargs):
+        keywords = kwargs.copy()
+        opt_keys = (
+            "hook",
+            "group",
+            "save",
+            "sync",
+            "cached",
+            "ack_failure",
+            "iter_count",
+            "iter_cached",
+            "chain",
+            "broker",
+            "timeout",
+        )
+        q_options = keywords.pop("q_options", {})
+        # get an id
+        tag = uuid()
+        # build the task package
+        task = {
+            "id": tag[1],
+            "name": keywords.pop("task_name", None)
+            or q_options.pop("task_name", None)
+            or tag[0],
+            "func": func,
+            "args": args,
+        }
+        # push optionals
+        for key in opt_keys:
+            if q_options and key in q_options:
+                task[key] = q_options[key]
+            elif key in keywords:
+                task[key] = keywords.pop(key)
+        # don't serialize the broker
+        broker = task.pop("broker", get_broker())
+        # overrides
+        if "cached" not in task and Conf.CACHED:
+            task["cached"] = Conf.CACHED
+        if "sync" not in task and Conf.SYNC:
+            task["sync"] = Conf.SYNC
+        if "ack_failure" not in task and Conf.ACK_FAILURES:
+            task["ack_failure"] = Conf.ACK_FAILURES
+        # finalize
+        task["kwargs"] = keywords
+        task["started"] = timezone.now()
+        # signal it
+        pre_enqueue.send(sender="django_q", task=task)
+        # sign it
+        pack = SignedPackage.dumps(task)
+        return tag, task, broker, pack
+
+    @staticmethod
     def add_async_task(func, *args, **kwargs):
+        # Wrapper method to add a task with awareness of schemapack
         if "schema_name" not in kwargs:
             kwargs.update({"schema_name": connection.schema_name})
-        return async_task(func, *args, **kwargs)
+
+        tag, task, broker, pack = QUtilities.prepare_task(func, *args, **kwargs)
+        if task.get("sync", False):
+            return QUtilities.run_synchronously(pack)
+
+        enqueue_id = broker.enqueue(pack)
+        logger.info(f"Enqueued {enqueue_id}")
+        logger.debug(f"Pushed {tag}")
+        return task["id"]
 
     @staticmethod
     def create_schedule(func, *args, **kwargs):
+        # Wrapper method to create schedule with awareness of schema
         schema_name = kwargs.get("schema_name", connection.schema_name)
         if schema_name:
             with schema_context(schema_name):
@@ -29,44 +107,126 @@ class QUtilities(object):
 
     @staticmethod
     def get_result(task_id, wait=0, cached=Conf.CACHED):
+        # Wrapper method to get result of a task with awareness of schema
         schema_name = connection.schema_name
         with schema_context(schema_name):
             return result(task_id, wait, cached)
 
     @staticmethod
     def get_result_group(group_id, failures=False, wait=0, count=None, cached=Conf.CACHED):
+        # Wrapper method to get result of a group with awareness of schema
         schema_name = connection.schema_name
         with schema_context(schema_name):
             return result_group(group_id, failures=False, wait=0, count=None, cached=Conf.CACHED)
 
     @staticmethod
     def fetch_task(task_id, wait=0, cached=Conf.CACHED):
+        # Wrapper method to fetch a single task with awareness of schema
         schema_name = connection.schema_name
         with schema_context(schema_name):
             return fetch(task_id, wait, cached)
 
     @staticmethod
     def fetch_task_group(group_id, failures=True, wait=0, count=None, cached=Conf.CACHED):
+        # Wrapper method to get a group with tasks with awareness of schema
         schema_name = connection.schema_name
         with schema_context(schema_name):
             return fetch_group(group_id, failures, wait, count, cached=cached)
 
     @staticmethod
     def get_group_count(group_id, failures=False, cached=Conf.CACHED):
+        # Wrapper method to get count of groups with awareness of schema
         schema_name = connection.schema_name
         with schema_context(schema_name):
             return count_group(group_id, failures, cached)
 
     @staticmethod
     def delete_task_group(group_id, tasks=False, cached=Conf.CACHED):
+        # Wrapper method to delete task group with awareness of schema
         schema_name = connection.schema_name
         with schema_context(schema_name):
             return delete_group(group_id, tasks, cached)
 
     @staticmethod
     def delete_task_from_cache(task_id, broker=None):
+        # Wrapper method to delete a task from the cache
         return delete_cached(task_id, broker=broker)
 
     @staticmethod
     def get_queue_size(broker=None):
+        # Wrapper method to get the queue size
         return queue_size(broker)
+
+    @staticmethod
+    def add_async_tasks_from_iter(func, args_iter, **kwargs):
+        """
+        Wrapper around async_iter that enqueues a function with iterable arguments
+        """
+        iter_count = len(args_iter)
+        iter_group = uuid()[1]
+
+        # clean up the kwargs
+        options = kwargs.get("q_options", kwargs)
+        options.pop("hook", None)
+        options["broker"] = options.get("broker", get_broker())
+        options["group"] = iter_group
+        options["iter_count"] = iter_count
+        if options.get("cached", None):
+            options["iter_cached"] = options["cached"]
+        options["cached"] = True
+        # save the original arguments
+        broker = options["broker"]
+        broker.cache.set(
+            f"{broker.list_key}:{iter_group}:args", SignedPackage.dumps(args_iter)
+        )
+
+        for args in args_iter:
+            if not isinstance(args, tuple):
+                args = (args,)
+            QUtilities.add_async_task(func, *args, **options)
+        return iter_group
+
+    @staticmethod
+    def create_async_tasks_chain(chain, group=None, cached=Conf.CACHED, sync=Conf.SYNC, broker=None):
+        """
+        Wrapper method around async_chain that enqueues a chain of tasks
+        the chain must be in the format [(func,(args),{kwargs}),(func,(args),{kwargs})]
+        """
+        if not group:
+            group = uuid()[1]
+        args = ()
+        kwargs = {}
+        task = chain.pop(0)
+        if type(task) is not tuple:
+            task = (task,)
+        if len(task) > 1:
+            args = task[1]
+        if len(task) > 2:
+            kwargs = task[2]
+        kwargs["chain"] = chain
+        kwargs["group"] = group
+        kwargs["cached"] = cached
+        kwargs["sync"] = sync
+        kwargs["broker"] = broker or get_broker()
+        QUtilities.add_async_task(task[0], *args, **kwargs)
+        return group
+
+    @staticmethod
+    def run_synchronously(pack):
+        """Method to run a task synchoronously"""
+
+        from django_tenant_schemas_q.cluster import worker, monitor
+
+        task_queue = Queue()
+        result_queue = Queue()
+        task = SignedPackage.loads(pack)
+        task_queue.put(task)
+        task_queue.put("STOP")
+        worker(task_queue, result_queue, Value("f", -1))
+        result_queue.put("STOP")
+        monitor(result_queue)
+        task_queue.close()
+        task_queue.join_thread()
+        result_queue.close()
+        result_queue.join_thread()
+        return task["id"]
